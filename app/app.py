acf3d1c01f0df490cdd022a679f2c49f0362ef4b
Gudisa Gebi <gudisagebi1@gmail.com>
2024-09-12 06:15:55 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! IT is me Gudisa'
+    return 'Hello, World! IT is me Gudisa Gebi'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
